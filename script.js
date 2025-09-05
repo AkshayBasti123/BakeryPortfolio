067415980c7c52239441322820698919e14a0959
AkshayBasti <70245648+AkshayBasti123@users.noreply.github.com>
2025-09-05 00:17:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,23 @@ document.querySelectorAll('.nav-links a').forEach(anchor => {
       .scrollIntoView({ behavior: 'smooth' });
   });
 });
+
+const cookieCard = document.getElementById('cookieCard');
+const acceptBtn = document.querySelector('.acceptButton');
+const declineBtn = document.querySelector('.declineButton');
+
+acceptBtn.addEventListener('click', () => {
+  cookieCard.style.display = 'none';
+  localStorage.setItem('cookieConsent', 'accepted');
+});
+
+declineBtn.addEventListener('click', () => {
+  cookieCard.style.display = 'none';
+  localStorage.setItem('cookieConsent', 'declined');
+});
+
+window.addEventListener('load', () => {
+  if (localStorage.getItem('cookieConsent')) {
+    cookieCard.style.display = 'none';
+  }
+});
